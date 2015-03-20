7c924279aea66e24c55d074a3ff39d6224dc900b
bmazen <benjamin@zenexity.com>
2015-03-20 13:48:21 +0100

    texte

--- src/Review.js
+++ src/Review.js
@@ -6,6 +6,12 @@ var style = {
 
 };
 
+var H1= {
+
+padding:'16px 0 9px'
+
+};
+
 var Review = React.createClass({
   render: function() {
     var data = this.props.data;
@@ -14,7 +20,7 @@ var Review = React.createClass({
         <div>
           <Tag>{data.tag}</Tag>
           <ProfilePicture url={data.author.picture} />
-          <h1>{data.title}</h1>
+          <h1 style={H1}>{data.title}</h1>
           <p>{data.content}</p>
         </div>
       </article>
